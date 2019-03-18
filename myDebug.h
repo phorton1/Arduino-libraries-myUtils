556086b8dd2d9b42160fba398f379efd38abfc1b
Patrick Horton <available@upon.request>
2019-03-18 11:30:26 -0500

    checkpoint initial work on looper

--- myDebug.h
+++ myDebug.h
@@ -58,8 +58,8 @@
         
 #ifdef CORE_TEENSY    
     // use the hardware Serial1 port on TEENSY
-    #define USE_SERIAL1  1
-    #define MY_BAUD_RATE  921600    // 921600, 460800, 230400, 115200
+    #define USE_SERIAL1  0
+    #define MY_BAUD_RATE  115200    // 921600    // 921600, 460800, 230400, 115200
     #undef USE_MEMORY_CHECK
 #else
     #define USE_SERIAL1  0
